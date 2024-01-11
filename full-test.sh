f03a392734d8aa418a5c448950d1a484f0dee609
GlennWSo <sondellglenn@gmail.com>
2024-01-11 21:05:02 +0100

    let env define path to bash

--- full-test.sh
+++ full-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script can be copied into your base directory for use with
 # automated testing using assignment-autotest.  It automates the
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
