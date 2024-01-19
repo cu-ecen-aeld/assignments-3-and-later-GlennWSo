557ec6f2e39bc36c97e84149c3ab832bbbee8094
GlennWSo <sondellglenn@gmail.com>
2024-01-19 11:01:23 +0100

    print error info

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main() {
+int main(int argc, char *argv[]) {
+   
+   argc -= 1;
+
+   if (argc != 2) {
+      printf("%s requires 2 args\n", argv[0]);
+      printf("arg count is: %i\n", argc);
+      for (int i=1; i < argc + 1; i++) {
+         printf("arg%i: %s\n", i, argv[i]);
+       }
+      return 1;
+   }
+   
    FILE *file;
    file = fopen("tmp/hello.txt", "w");
    fprintf(file, "Hello, World!");
