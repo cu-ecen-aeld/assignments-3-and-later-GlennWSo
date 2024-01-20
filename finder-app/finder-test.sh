2f4d6bc0778149c9a27212e34d4e3a1fe212493b
GlennWSo <sondellglenn@gmail.com>
2024-01-20 09:43:41 +0100

    use native

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,14 +47,15 @@ then
 	else
 		exit 1
 	fi
+else
+	echo "Removing the old writer utility and compiling as a native application"
+	make clean
+	make
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
