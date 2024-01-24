6135c4a5e9b1e093af4d1b5257a74ea032fabae4
GlennWSo <sondellglenn@gmail.com>
2024-01-24 10:35:08 +0100

    impl do_system

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,6 +16,7 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int res = system(cmd);
 
     return true;
 }
