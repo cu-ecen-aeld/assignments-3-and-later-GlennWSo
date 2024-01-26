ce275311f0b4c4fc3dc57957171b5a8eef1b68fe
GlennWSo <sondellglenn@gmail.com>
2024-01-26 11:35:45 +0100

    kernal image built

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,7 +5,7 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
+OUTDIR=out #/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
@@ -24,38 +24,52 @@ fi
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
+if [ ! -d "linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+if [ ! -e linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
     echo starting kernel build
-    # step 0: deep clean, removing the .config file with any existing configs
-    make mrproper
-    # create a default .config
-    make defconfig
-    echo derp
-    exit 0
+    echo deep clean, removing the .config file with any existing configs
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
 
+    echo creating default config
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig # create a default .config
+
+    echo building kernal image
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j4 all # make kernal image
+
+    echo building modules
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+
+    echo building device tree
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
+
+    echo linux kernel build fin
 fi
 
 echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
+if [ -d "rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+echo Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir bin dev etc home lib lib64 proc sbin sys tmp
+mkdir -p usr/bin usr/lib usr/sbin var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
