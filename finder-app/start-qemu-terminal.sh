c24ac0a21d6164263299ee1a99a80084aef9d0c1
GlennWSo <sondellglenn@gmail.com>
2024-01-29 11:09:39 +0100

    pretty args

--- finder-app/start-qemu-terminal.sh
+++ finder-app/start-qemu-terminal.sh
@@ -26,7 +26,13 @@ fi
 
 echo "Booting the kernel"
 # See trick at https://superuser.com/a/1412150 to route serial port output to file
-qemu-system-aarch64 -m 256M -M virt -cpu cortex-a53 -nographic -smp 1 -kernel ${KERNEL_IMAGE} \
-        -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
-        -serial chardev:char0 -mon chardev=char0\
-        -append "rdinit=/bin/sh" -initrd ${INITRD_IMAGE}
+qemu-system-aarch64 \
+    -m 256M \
+    -M virt \
+    -append "rdinit=/bin/sh" -initrd ${INITRD_IMAGE}
+    -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
+    -cpu cortex-a53 \
+    -kernel ${KERNEL_IMAGE} \
+    -nographic \
+    -serial chardev:char0 -mon chardev=char0 \
+    -smp 1 \
