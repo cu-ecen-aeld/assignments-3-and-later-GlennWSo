786210550a7013c15b8a982c87a2551719b42719
GlennWSo <sondellglenn@gmail.com>
2024-01-29 11:10:20 +0100

    sort args

--- finder-app/start-qemu-app.sh
+++ finder-app/start-qemu-app.sh
@@ -27,12 +27,12 @@ fi
 echo "Booting the kernel"
 # See trick at https://superuser.com/a/1412150 to route serial port output to file
 qemu-system-aarch64 \
-        -m 256M \
-        -M virt \
-        -cpu cortex-a53 \
-        -nographic \
-        -smp 1 \
-        -kernel ${KERNEL_IMAGE} \
-        -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
-        -serial chardev:char0 -mon chardev=char0 \
-        -append "rdinit=/home/autorun-qemu.sh console=ttyAMA0" -initrd ${INITRD_IMAGE}
+    -M virt \
+    -append "rdinit=/home/autorun-qemu.sh console=ttyAMA0" -initrd ${INITRD_IMAGE}
+    -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
+    -cpu cortex-a53 \
+    -kernel ${KERNEL_IMAGE} \
+    -m 256M \
+    -nographic \
+    -serial chardev:char0 -mon chardev=char0 \
+    -smp 1 \
