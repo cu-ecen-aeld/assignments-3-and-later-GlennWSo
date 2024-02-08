fd97f1e8c16a00c77eee90ae249b7188aa953851
GlennWSo <sondellglenn@gmail.com>
2024-02-08 13:33:24 +0100

    progress

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,3 +1,4 @@
+#include "sys/syslog.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,33 +8,65 @@
 
 
 int main(){
-	int domain, type, protocol;
-	domain = AF_LOCAL;
-	type = SOCK_STREAM;
-	protocol = 0; // IP
-	
-	
-	int sockfd = socket(domain, type, protocol);
+	// int domain, type, protocol;
+	// domain = AF_LOCAL;
+	// type = SOCK_STREAM;
+	// int protocol = 0; // IP
+   openlog(NULL, 0, LOG_USER);
 
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
+	// hints.ai_protocol = 0;
 	
 	struct addrinfo *servinfo;
   char port[5] = "1234";
   int res = getaddrinfo(NULL, port, &hints, &servinfo);
 	if (res != 0 ) {
-		perror("getaddrinfo failed");
+		// perror("getaddrinfo failed");
+		fprintf(stderr, "getaddrinfo failed: %s", strerror(-res));
 		exit(1);
 	}
+	printf("get addres ok\n");
 
-	res = bind(sockfd, servinfo->ai_addr , servinfo->ai_addrlen);
+	int sockfd = socket(
+		servinfo->ai_family,
+		// AF_LOCAL,
+		servinfo->ai_socktype,
+		servinfo->ai_protocol
+	);
+	if (sockfd == -1 ) {
+		perror("socket failed");
+		exit(1);
+	}
+   syslog(LOG_INFO, "sock ok\n");
+
+
+	res = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
 	if (res != 0 ) {
 		perror("bind failed");
 		exit(1);
 	}
+   syslog(LOG_INFO, "bind ok\n");
+
+	res = listen(sockfd, 10);
+	if (res != 0 ) {
+		perror("listen failed");
+		exit(1);
+	}
+
+   syslog(LOG_INFO, "listen ok\n");
+
+	struct sockaddr_storage client_addr;
+	socklen_t addr_size = sizeof(client_addr);
+	int accfc = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+	if (res == -1 ) {
+		perror("acc failed");
+		exit(1);
+	}
+   syslog(LOG_INFO, "acc ok\n");
 
 
 	freeaddrinfo(servinfo);
